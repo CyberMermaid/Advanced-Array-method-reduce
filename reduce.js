b3ff13c1a8d181fdccabe8829613036e358e35c7
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2023-07-23 12:44:52 -0400

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,110 @@
+/*
+Write a function called extractValue which accepts an array of objects and a key and returns a new array with the value of each object at the key.
+
+Examples:
+    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}]
+    extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
+*/
+
+function extractValue(arr, key) {
+    return arr.reduce(function (accumulator, currentValue) {
+        accumulator.push(currentValue[key]);
+        return accumulator;
+    }, []);
+}
+
+/*
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+
+Examples:
+    vowelCount('Elie') // {e:2,i:1};
+    vowelCount('Tim') // {i:1};
+    vowelCount('Matt') // {a:1})
+    vowelCount('hmmm') // {};
+    vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
+*/
+
+function vowelCount(str) {
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    let word = str.toLowerCase().split('');
+
+    return word.reduce(function (accumulator, currentValue) {
+        if (vowels.includes(currentValue)) {
+            // If the vowel exists in the object accumulator as a key/currentValue, then increase by 1.
+            // Otherwise, create key for currentValue in accumulator, assign it the value zero, then increase by 1.
+            accumulator[currentValue] = (accumulator[currentValue] || 0) + 1
+        }
+        return accumulator;
+    },
+        {});
+}
+
+/*
+Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+
+Examples:
+    const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
+    
+    addKeyAndValue(arr, 'title', 'Instructor') // 
+      [
+        {title: 'Instructor', name: 'Elie'}, 
+        {title: 'Instructor', name: 'Tim'}, 
+        {title: 'Instructor', name: 'Matt'}, 
+        {title: 'Instructor', name: 'Colt'}
+       ]
+*/
+
+function addKeyAndValue(arr, key, value) {
+    let arrWithKeyValue = arr.reduce(function (acc, obj) {
+        // Set new Object = key value pair followed by key value pairs from obj via the spread operator 
+        const newObj = { [key]: value, ...obj };
+        //Push newObj into acc which was initialized to an empty array
+        acc.push(newObj);
+        return acc;
+    }, []);
+    // Display arrWithKeyValue
+    console.log(arrWithKeyValue);
+    return arrWithKeyValue;
+}
+
+/*
+Write a function called partition which accepts an array and a callback and returns an array with two
+arrays inside of it. The partition function should run the callback function on each value in the array
+and if the result of the callback function at that specific value is true, the value should be placed in
+the first subarray. If the result of the callback function at that specific value is false, the value
+should be placed in the second subarray.
+
+Examples:
+
+    function isEven(val){
+        return val % 2 === 0;
+    }
+
+    const arr = [1,2,3,4,5,6,7,8];
+
+    partition(arr, isEven) // [[2,4,6,8], [1,3,5,7]];
+
+    function isLongerThanThreeCharacters(val){
+        return val.length > 3;
+    }
+
+    const names = ['Elie', 'Colt', 'Tim', 'Matt'];
+
+    partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
+*/
+
+
+
+function partition(arr, callback) {
+    return arr.reduce(function (acc, el) {
+        //If callback returns true, push the element el into the first array of acc.
+        if (callback(el)) {
+            acc[0].push(el);
+        }
+        //Otherwise, push the element el into the second array of acc.
+        else {
+            acc[1].push(el);
+        }
+        return acc;
+    }, [[], []]);
+}
